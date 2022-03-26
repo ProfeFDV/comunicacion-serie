e36fcf2523d1930598e39380275a14ef4bc288f1
Fran Diaz <100407685+ProfeFDV@users.noreply.github.com>
2022-03-26 15:20:47 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,28 @@
+input.onButtonPressed(Button.A, function () {
+    a += 1
+})
+input.onButtonPressed(Button.B, function () {
+    a += -1
+})
 let x = 0
 let y = 0
 let a = 0
 let b = 0
 basic.forever(function () {
-    x = randint(0, 5)
-    y = randint(0, 5)
-    a = randint(0, 5)
-    b = randint(0, 5)
+    x = Math.round(pins.map(
+    pins.analogReadPin(AnalogPin.P1),
+    0,
+    1023,
+    0,
+    4
+    ))
+    y = Math.round(pins.map(
+    pins.analogReadPin(AnalogPin.P0),
+    0,
+    1023,
+    0,
+    4
+    ))
     serial.writeLine("" + x + "," + y + "," + a + "," + b)
     basic.pause(1000)
 })
